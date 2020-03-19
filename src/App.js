3d571f7cf04a03bb140430c52b9b69e609c2ab64
DougCaswell <dougcaswell@gmail.com>
2020-03-19 12:01:44 -0600

    made static list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Todo
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: ['task']
+    }
+  }
+
+  //   async componentDidMount() {
+  //     let res = await axios.get('/api/employees');
+  //     this.setState({
+  //         employees: res.data
+  //     });
+  // }
+
+  getItems() {
+    let list = this.state.items
+    let mappedList = list.map((task, i) => (
+      <li key={i}>{task}</li>
+    ))
+    return (
+      <ul className="list">
+        {mappedList}
+      </ul>
+    )
+  }
+  render() {
+    return (
+      <div className="App" >
+        <h1>Todo</h1>
+        {this.getItems()}
+      </div>
+    )
+  };
 }
 
 export default App;
